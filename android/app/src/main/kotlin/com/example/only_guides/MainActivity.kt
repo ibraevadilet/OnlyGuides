3cfc337ee8c71b4f90fd41fd1b40163fd7637636
adilet <ibraevadilet3@gmail.com>
2023-04-09 16:22:21 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/only_guides/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.only_guides
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config/app_config.dart
@@ -0,0 +1,21 @@
+class AppConfig {
+  static const String appStoreIdentifier = '1671925193';
+  static const String apphudKey = 'app_bcbWL1h5yAXJYQDrSY3uKQf6Xq7AL8';
+  static const String privacyPolicy = 'https://docs.google.com/document/d/1Y-WYTg-5d7yykd0ttWFp3A0qWLOcozik7IVRVMdX8wM/edit?usp=sharing';
+  static const String termOfUse = 'https://docs.google.com/document/d/13psrnVU4DvX4S6YC21fmtD7ABG_Z57yDwTKNyhalqrE/edit?usp=sharing';
+  static const String supportForm = 'https://sites.google.com/view/travlin/support-form';
+  static const String databaseRoot = 'json_root';
+  static const String baseUrl1 = 'travlin';
+  static const String baseUrl2 = 'app';
+  static const String url11key = 'travel';
+  static const String url12key = 'premium';
+  static const String url21key = 'posts';
+  static const String url22key = 'publicate';
+  static const String firebaseApiKey = 'AIzaSyAM4ZMawdCwHoSF3VGGpWkvRdnZLaTGIqU';
+  static const String firebaseAppId = '1:95499598839:ios:c5671de71533c20673ccf2';
+  static const String firebaseMessagingSenderId = '95499598839';
+  static const String firebaseProjectId = 'travlin-735d7';
+  static const String firebaseStorageBucket = 'travlin-735d7.appspot.com';
+}
+
+
